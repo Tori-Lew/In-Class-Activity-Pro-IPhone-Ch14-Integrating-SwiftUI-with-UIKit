d00c275ecddcad18e80c6f351ff54ef1b0aa191c
Tori Lew <55900873+Tori-Lew@users.noreply.github.com>
2023-12-04 16:46:07 -0800

    Initial Commit

---
+++ In-Class Activity Pro IPhone Ch14 Integrating SwiftUI with UIKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  In-Class Activity Pro IPhone Ch14 Integrating SwiftUI with UIKit
+//
+//  Created by Student Account on 12/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ In-Class Activity Pro IPhone Ch14 Integrating SwiftUI with UIKit/In_Class_Activity_Pro_IPhone_Ch14_Integrating_SwiftUI_with_UIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  In_Class_Activity_Pro_IPhone_Ch14_Integrating_SwiftUI_with_UIKitApp.swift
+//  In-Class Activity Pro IPhone Ch14 Integrating SwiftUI with UIKit
+//
+//  Created by Student Account on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct In_Class_Activity_Pro_IPhone_Ch14_Integrating_SwiftUI_with_UIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
